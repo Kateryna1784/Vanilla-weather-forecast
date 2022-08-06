8dc522fafa70a64f1f7c57c03aab22f515d2a111
Kateryna1784 <109739380+Kateryna1784@users.noreply.github.com>
2022-08-06 17:08:52 +0300

    added array of daily temperatures

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast () {
+function displayForecast (response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast")
   let forecastHTML =`<div class = "row">`;
   let dayss = ["Thu", "Fri", "Sat", "Sun","Mon"];
@@ -47,6 +48,12 @@ function displayForecast () {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7f5a3748cbe33e0ca87d6a6fcae64515";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 let now = new Date();
 let currentTime = document.querySelector("#time");
@@ -69,7 +76,7 @@ let locationButton = document.querySelector("#geoButton");
 locationButton.addEventListener("click", showPosition);
 searchCity("Kyiv");
 
-displayForecast();
+//displayForecast();
 
 function submitCity(event) {
   event.preventDefault();
@@ -103,6 +110,8 @@ iconElement.setAttribute(
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 );
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
 }
 
 
